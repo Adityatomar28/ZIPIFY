3f407848f738c7192af4fe732eb11789b802ce95
Adityatomar28 <adityasinghtomar52gmail.com>
2026-03-22 05:13:38 +0530

    Added Cloudinary image upload API with authentication

---
+++ prisma/migrations/20260321195730_init/migration.sql
@@ -0,0 +1,22 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Video` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "Video";
+
+-- CreateTable
+CREATE TABLE "video" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "publicId" TEXT NOT NULL,
+    "originalSize" TEXT NOT NULL,
+    "duration" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "video_pkey" PRIMARY KEY ("id")
+);
